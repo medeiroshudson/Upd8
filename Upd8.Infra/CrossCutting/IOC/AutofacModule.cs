1121d277e627850dd9912b34b5220364d840bcbc
medeiroshudson <medeiroshudson@outlook.com>
2023-02-13 19:29:03 -0300

    Adicionado: Autofac para Injeção de Dependencias

---
+++ Upd8.Infra/CrossCutting/IOC/AutofacModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Autofac;
+using Upd8.Application.Services;
+using Upd8.Domain.Interfaces.Repositories;
+using Upd8.Domain.Interfaces.Services;
+using Upd8.Infra.Data.Repositories;
+
+namespace Upd8.Infra.CrossCutting.IOC
+{
+	public class AutofacModule : Module
+	{
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CustomerService>().As<ICustomerService>();
+            builder.RegisterType<CustomerRepository>().As<ICustomerRepository>();
+        }
+    }
+}
+
---
+++ Upd8.Presentation/Upd8.Api/Extensions/ConfigureAutofac.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Upd8.Infra.CrossCutting.IOC;
+
+namespace Upd8.Api.Extensions
+{
+    public static class ConfigureAutofac
+    {
+        public static IHostBuilder AddAutofacModule(this WebApplicationBuilder builder)
+        {
+            return builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureContainer<ContainerBuilder>(builder =>
+                {
+                    builder.RegisterModule(new AutofacModule());
+                });
+        }
+    }
+}
+
