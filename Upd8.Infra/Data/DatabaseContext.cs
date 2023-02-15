cf380197b41074e179bf68578bf03d8a0d98d5bc
medeiroshudson <medeiroshudson@outlook.com>
2023-02-14 21:53:14 -0300

    Fix: Ajustado a declaração do DbSet Customer

--- Upd8.Infra/Data/DatabaseContext.cs
+++ Upd8.Infra/Data/DatabaseContext.cs
@@ -6,57 +6,58 @@ using Upd8.Domain.Core.Entities;
 
 namespace Upd8.Infra.Data
 {
-	public class DatabaseContext : DbContext
-	{
-		public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
-		public DbSet<Customer> Customer { get; set; }
+        public DbSet<Customer> Customer { get; set; } = null!;
 
         public override int SaveChanges(bool acceptAllChangesOnSuccess)
         {
-			var entries = ChangeTracker.Entries()
-				.Where(x => x.Entity is Entity)
-				.ToList();
+            var entries = ChangeTracker.Entries()
+                .Where(x => x.Entity is Entity)
+                .ToList();
 
             UpdateTimestamps(entries);
-			ConfigureSoftDelete(entries);
+            ConfigureSoftDelete(entries);
 
             return base.SaveChanges(acceptAllChangesOnSuccess);
         }
 
-		private static void ConfigureSoftDelete(List<EntityEntry> entries)
-		{
-			var filteredEntities = entries.Where(x =>
+        private static void ConfigureSoftDelete(List<EntityEntry> entries)
+        {
+            var filteredEntities = entries.Where(x =>
                     x.State == EntityState.Added ||
-					x.State == EntityState.Deleted
-				);
-
-			foreach(var entry in filteredEntities)
-			{
-				if (entry.State == EntityState.Added)
-				{
-                    ((Entity)entry.Entity).Deleted = false;
-                } else if (entry.State == EntityState.Deleted)
-				{
-                    entry.State = EntityState.Modified;
-                    ((Entity)entry.Entity).Deleted = true;
-                    ((Entity)entry.Entity).UpdatedAt = DateTime.Now;
-                }
-			}
-		}
-
-		private static void UpdateTimestamps(List<EntityEntry> entries)
-		{
-			var filteredEntities = entries.Where(x =>
-					x.State == EntityState.Added ||
-					x.State == EntityState.Modified
-				);
+                    x.State == EntityState.Deleted
+                );
 
             foreach (var entry in filteredEntities)
             {
                 if (entry.State == EntityState.Added)
                 {
-					((Entity)entry.Entity).CreatedAt = DateTime.Now;
+                    ((Entity)entry.Entity).Deleted = false;
+                }
+                else if (entry.State == EntityState.Deleted)
+                {
+                    entry.State = EntityState.Modified;
+                    ((Entity)entry.Entity).Deleted = true;
+                    ((Entity)entry.Entity).UpdatedAt = DateTime.Now;
+                }
+            }
+        }
+
+        private static void UpdateTimestamps(List<EntityEntry> entries)
+        {
+            var filteredEntities = entries.Where(x =>
+                    x.State == EntityState.Added ||
+                    x.State == EntityState.Modified
+                );
+
+            foreach (var entry in filteredEntities)
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    ((Entity)entry.Entity).CreatedAt = DateTime.Now;
                 }
 
                 ((Entity)entry.Entity).UpdatedAt = DateTime.Now;
